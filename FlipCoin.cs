7d0dd3123110b2b4e4e0f47fbbae2f3707725203
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-13 08:05:24 +0530

    The program for flip coin

---
+++ FlipCoin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flip_coin
+{
+    public class FlipCoin
+    {
+        public static void FlipTheCoin()
+        {
+            int headcount = 0;
+            int tailcount = 0;
+
+
+            Console.WriteLine("please enter the number of flip times");
+            int flip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(flip +" times coine will flip");
+            
+        for (int i = 0; i<flip ; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(2);
+                if (num == 0)
+                {
+                   tailcount++; 
+                }
+                else
+                {
+                   headcount++;
+                }
+            }
+            Console.WriteLine("head cout is "+ headcount );
+            Console.WriteLine("tail cout is "+ tailcount );
+
+            int headpercent = headcount * 100 / flip;
+            int tailpercent = tailcount * 100 / flip;
+
+            Console.WriteLine("head percentage is " + headcount +"%");
+            Console.WriteLine("head percentage is " + tailcount +"%");
+
+
+
+        }
+    }
+
+}
